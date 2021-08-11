11a2ff8b3bf01e92ebde0105a531617b2e0354e3
njhn-pnsb <najhan.najib@pipeline-network.com>
2021-08-12 07:54:58 +0800

    edit ui

--- resources/views/mesej.blade.php
+++ resources/views/mesej.blade.php
@@ -8,22 +8,22 @@
 <div class="row" style="margin-bottom: 30px;">
     <div class="col-lg-4">
         <a href="/aktif">
-            <button class="btn btn-success btn-block" style="height:120px">
+            <button class="btn btn-success btn-block" style="height:120px; border-radius: 12px;">
                 <h1>Aktif <i class="fa fa-bell" aria-hidden="true"></i></h1>
             </button>
         </a>
     </div>
     <div class="col-lg-4">
         <a href="/tugasans">
-            <button class="btn btn-success btn-block" style="height:120px">
+            <button class="btn btn-success btn-block" style="height:120px; border-radius: 12px;">
                 <h1>Tugasan <i class="fa fa-tasks" aria-hidden="true"></i></h1>
             </button>
         </a>
     </div>
     <div class="col-lg-4">
         <a href="/room">
-            <button class="btn btn-success btn-block" style="height:120px">
-                <h1>Semua <i class="fa fa-bell" aria-hidden="true"></i></h1>
+            <button class="btn btn-success btn-block" style="height:120px; border-radius: 12px;">
+                <h1>Semua <i class="fa fa-circle-o-notch" aria-hidden="true"></i></h1>
             </button>
         </a>
     </div>
@@ -63,9 +63,10 @@
             <form action="/hantarrr/{{$room['id']}}" method="POST">
                 @csrf
                 <select id="template" class="form-control" onchange="malas()">
-                    <option value=0 selected>Pilih Template</option>
+                    <option value=0 selected>Pilih Status</option>
                     <option value=5>Agensi</option>
                     <option value=6>Pertanyaan/Cadangan</option>
+                    <option value=7>Luar Bidang Kuasa</option>
                 </select>
                 <select id="templatela" class="form-control hide" onchange="malasla()">
                     <option value=0 selected>Pilih Template</option>
@@ -79,11 +80,11 @@
                     <option value=2>Isu Data Peribadi</option>
                     <option value=3>Status Aduan</option>
                 </select>
-                <select id="pertanyaan" class="form-control hide" onchange="subpertanyaan()">
+                <select id="luarbidang" class="form-control hide" onchange="subluarbidang()">
                     <option value=0 selected>Pilih Template</option>
-                    <option value=1>Isu Senarai Harga</option>
-                    <option value=2>Isu Data Peribadi</option>
-                    <option value=3>Status Aduan</option>
+                    <option value=1>JKDM - Jabatan Kastam Diraja Malaysia</option>
+                    <option value=2>SKMM - Suruhanjaya Komunikasi dan Multimedia Malaysia</option>
+                    <option value=3>CFM - Customer Forum Of Malaysia</option>
                 </select>
                 <!-- <div class="hide" id="hiden_div_catatan">
                     <h1>test</h1>
@@ -154,8 +155,12 @@
         } else if (x.value == 6) {
             // document.getElementById('templatela').style.display = "show";
             document.getElementById("pertanyaan").className = "form-control";
-
+        } else if (x.value == 7) {
+            // document.getElementById('templatela').style.display = "show";
+            document.getElementById("luarbidang").className = "form-control";
         }
+
+
         console.log(x.value)
     }
 
@@ -185,6 +190,21 @@
             y.value = 'Pihak Kementerian sangat menghargai keprihatinan Tuan/Puan untuk menegakkan *hak-hak pengguna* dan mengambil perhatian atas kesulitan yang berlaku. Untuk maklumat Tuan/Puan, pada lampiran muka surat 15 (http://www.pdp.gov.my/index.php/en/akta-709/akta-perlindungan-data-peribadi-2010) , data Tuan/Puan adalah diamanahkan untuk dilindungi dan tertakluk di bawah akta tersebut. Untuk makluman jua, maklumat lengkap diperlukan untuk menjaga integriti data dan memudahkan tindakan penguatkuasaan. Sehubungan itu, sila lengkap kan butiran berikut : a) Nama (Nama Penuh Mengikut K/P) b) No.K/P (Sila Nyatakan No K/P Yang Sah) c) Alamat surat menyurat d) No telefon untuk dihubungi e) Email (Sekiranya ada) f) Nama Premis g) Alamat Premis (Sila Nyatakan Alamat Lengkap Termasuk Daerah) h) Keterangan Aduan i) Gambar Bukti (Sekiranya Ada) j) Sekiranya Aduan Berkenaan Transaksi Atas Talian Mohon Kemukakan Nama Bank Dan No Akaun Bank / No Transaksi FPX Yang Terlibat Diharapkan agar isu tersebut akan diambil tindakan yang tepat. Terima kasih. - KPDNHEP #pdpa';
         } else if (x.value == 3) {
             y.value = 'Tuan/Puan, Aduan tersebut masih dalam siasatan. Sebarang pertanyaan boleh dirujuk kepada Pegawai Penyiasat yang bertanggungjawab menjalankan siasatan ke atas aduan tuan. Pegawai Penyiasat : xxx, No.Telefon : xxx, E-mel : xxx. #status';
+        }
+        console.log(x.value)
+    }
+
+    function subluarbidang() {
+        var x = document.getElementById("luarbidang");
+        var y = document.getElementById("chatbox");
+
+
+        if (x.value == 1) {
+            y.value = 'Tuan/ Puan, Berdasarkan semakan, didapati aduan/pertanyaan tuan/puan adalah di luar bidang kuasa KPDNHEP dan lebih menjurus kepada bidang kuasa *JABATAN KASTAM DIRAJA MALAYSIA* http://aduan.customs.gov.my/aduanawam/index1.php Sila rujuk pautan tersebut untuk perhatian selanjutnya. Sekian, terima kasih KPDNHEP #kastam';
+        } else if (x.value == 2) {
+            y.value = 'Tuan/ Puan, Berdasarkan semakan, didapati aduan/pertanyaan tuan/puan adalah di luar bidang kuasa KPDNHEP dan lebih menjurus kepada bidang kuasa *SURUHANJAYA KOMUNIKASI DAN MULTIMEDIA MALAYSIA* https://aduan.skmm.gov.my/ Sila rujuk pautan tersebut untuk perhatian selanjutnya. Sekian, terima kasih. -KPDNHEP #skmm';
+        } else if (x.value == 3) {
+            y.value = 'Tuan/ Puan, Berdasarkan semakan, didapati aduan/pertanyaan tuan/puan adalah di luar bidang kuasa KPDNHEP dan lebih menjurus kepada bidang kuasa *CONSUMER FORUM OF MALAYSIA* http://complaint.cfm.my/ Sila rujuk pautan tersebut untuk perhatian selanjutnya. Sekian, terima kasih. -KPDNHEP #cfm';
         }
         console.log(x.value)
     }
