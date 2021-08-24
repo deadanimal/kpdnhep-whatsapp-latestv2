406ce17e723fdcd8dd258c1f836c8cfd82b6cc38
njhn-pnsb <najhan.najib@pipeline-network.com>
2021-08-24 15:55:28 +0800

    remove amchart watermark

--- resources/views/laporanpegawai.blade.php
+++ resources/views/laporanpegawai.blade.php
@@ -324,6 +324,7 @@
 
         // Themes begin
         am4core.useTheme(am4themes_animated);
+        am4core.addLicense("ch-custom-attribution");
         // Themes end
 
         // Create chart instance
@@ -418,6 +419,7 @@
 
         // Themes begin
         am4core.useTheme(am4themes_animated);
+        am4core.addLicense("ch-custom-attribution");
         // Themes end
 
         // Create chart instance
